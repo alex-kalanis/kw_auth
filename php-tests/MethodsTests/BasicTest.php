27655f7b360e5ec9959a99d1751ac586d0121da3
Petr Plsek <me@kalanys.com>
2021-10-20 23:11:53 +0200

    kw_auth - testing files reading and writing

--- php-tests/MethodsTests/BasicTest.php
+++ php-tests/MethodsTests/BasicTest.php
@@ -4,11 +4,30 @@ namespace MethodsTests;
 
 
 use CommonTestClass;
+use kalanis\kw_auth\AuthException;
 use kalanis\kw_auth\Methods;
+use kalanis\kw_auth\Sources\TFiles;
+use kalanis\kw_auth\Sources\TLines;
 
 
 class BasicTest extends CommonTestClass
 {
+    protected $sourcePath = '';
+    protected $testingPath = '';
+
+    protected function setUp(): void
+    {
+        $this->sourcePath = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . '.groups';
+        $this->testingPath = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . '.groups-duplicate';
+    }
+
+    protected function tearDown(): void
+    {
+        if (is_file($this->testingPath)) {
+            unlink($this->testingPath);
+        }
+    }
+
     /**
      * @throws \kalanis\kw_auth\AuthException
      * @throws \kalanis\kw_locks\LockException
@@ -48,6 +67,62 @@ class BasicTest extends CommonTestClass
             [ time() - 50, 20, false],
         ];
     }
+
+    /**
+     * @param string $in
+     * @param string $want
+     * @dataProvider stripProvider
+     */
+    public function testStrip(string $in, string $want): void
+    {
+        $lib = new MockLines();
+        $this->assertEquals($want, $lib->stripChars($in));
+    }
+
+    public function stripProvider(): array
+    {
+        return [
+            ['yxcvbnm', 'yxcvbnm'],
+            ['jk~l,.qđwĐe', 'jkl,.qwe'],
+        ];
+    }
+
+    /**
+     * @param string $in
+     * @param string $want
+     * @dataProvider exImProvider
+     */
+    public function testExIm(string $in, string $want): void
+    {
+        $lib = new MockLines();
+        $this->assertEquals($want, $lib->implosion($lib->explosion($in)));
+    }
+
+    public function exImProvider(): array
+    {
+        return [
+            ['yxc:vb:nm', 'yxc:vb:nm'],
+            ['yxcvbnm', 'yxcvbnm'],
+            ['j°k~l:,.qđwĐe', 'j°k~l:,.qđwĐe'],
+        ];
+    }
+
+    public function testFiles(): void
+    {
+        $lib = new MockFiles();
+        $content = $lib->open($this->sourcePath);
+        $this->assertnotEmpty($content);
+        $lib->save($this->testingPath, $content);
+        $this->expectException(AuthException::class);
+        $lib->save($this->testingPath, $content);
+    }
+
+    public function testFilesOpenCrash(): void
+    {
+        $lib = new MockFiles();
+        $this->expectException(AuthException::class);
+        $lib->open($this->testingPath);
+    }
 }
 
 
@@ -83,5 +158,28 @@ class MockStamp
     public function checkTime(int $time): bool
     {
         return $this->checkStamp($time);
+    }
+}
+
+
+class MockLines
+{
+    use TLines;
+}
+
+
+class MockFiles
+{
+    use TFiles;
+    use TLines;
+
+    public function open(string $path): array
+    {
+        return $this->openFile($path);
+    }
+
+    public function save(string $path, array $content): void
+    {
+        $this->saveFile($path, $content);
     }
 }
